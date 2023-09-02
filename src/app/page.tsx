219b12f5f6750794ea70b2813089a06750330918
Arif <iariful950@gmail.com>
2023-09-03 03:12:16 +0600

    [DONE] authentication, navbar design almost there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <p className="text-red-500">Hello World!</p>;
+  return <Button>Hello World</Button>
 }
